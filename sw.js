58047c68a6dba32774265f64422f308a51c4846e
getprettypenny <getprettypenny@gmail.com>
2026-05-10 09:14:06 -0700

    Add files via upload
    added PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'routine-v1';
+const ASSETS = [
+  '/my-routine/',
+  '/my-routine/index.html',
+  '/my-routine/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
